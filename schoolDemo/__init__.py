b12978f59f766de5adfcdbe2194ec1a6fb21194b
gerardo <gerardo.delarosma@udlap.mx>
2016-05-26 00:06:07 -0500

    Adding admin module

--- schoolDemo/__init__.py
+++ schoolDemo/__init__.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask.ext.triangle import Triangle
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+mongo = PyMongo(app)
+
 # Adding Triangle in order to use AngularJS
 Triangle(app)
 
--- schoolDemo/views.py
+++ schoolDemo/views.py
@@ -1,7 +1,10 @@
 # coding=utf-8
-from schoolDemo import app
+from schoolDemo import app, mongo
+from flask import Flask
 from flask import jsonify, render_template, g, flash, redirect, url_for, json
-
+from bson.objectid import ObjectId
+from decimal import Decimal
+from flask.ext.restful import Api, Resource, reqparse
 
 @app.route('/')
 def login():
@@ -22,3 +25,7 @@ def teacher_class():
 @app.route('/student/class')
 def student_class():
     return render_template("student_dashboard.html")
+
+@app.route('/admin')
+def admin():
+    return render_template("admin_base.html")
