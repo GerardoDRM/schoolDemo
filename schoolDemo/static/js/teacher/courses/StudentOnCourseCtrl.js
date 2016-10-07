7c4a2b453dec2c9738749a101ae0ff5c574b74ac
gerardo <gerardo.delarosma@udlap.mx>
2016-10-07 09:25:57 -0500

    Removing chat

--- schoolDemo/static/js/teacher/courses/StudentOnCourseCtrl.js
+++ schoolDemo/static/js/teacher/courses/StudentOnCourseCtrl.js
@@ -2,13 +2,25 @@ angular.module('SchoolApp').controller('StudentOnCourseCtrl', ['$scope', '$compi
   $scope.students = [];
   $scope.id = $("#courseId").val();
   $scope.section = 1;
+  var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septimbre", "Octubre", "Noviembre", "Diciembre"];
 
   $('#studentsCtrl').click(function() {
     $scope.students = [];
     $scope.getStudents();
   });
 
+  /**
+   * This function gets students attendance by month and
+   * It should has a editable tamplate in order to update
+   * attendance
+   **/
   $scope.getStudents = function() {
+    // Add Days in month
+    $scope.current_month = months[moment().month()] + " " + moment().year();
+    for (var i = 1; i <= moment().daysInMonth(); i++) {
+      _addDaysInMonth(i);
+    }
+
     $("#students-content").empty();
     $http({
       method: 'GET',
@@ -21,6 +33,11 @@ angular.module('SchoolApp').controller('StudentOnCourseCtrl', ['$scope', '$compi
     }, function errorCallback(response) {});
   }
 
+  var _addDaysInMonth = function(day) {
+    angular.element(document.getElementById('month-dates')).append($compile(
+      '<div class="date-box">' + day + '</div>'
+    )($scope));
+  }
   var _addStudent = function(stu) {
     angular.element(document.getElementById('students-content')).append($compile(
       '<tr class="md-row">' +
@@ -29,5 +46,18 @@ angular.module('SchoolApp').controller('StudentOnCourseCtrl', ['$scope', '$compi
       '<td>' + stu.name + '</td>' +
       '</tr>'
     )($scope));
+    // angular.element(document.getElementById('students-content')).append($compile(
+    //   '<tr class="md-row">' +
+    //   '<td class="medium-box">' + stu._id + '</td>' +
+    //   '<td class="medium-box">' + stu.name + '</td>' +
+    //   '<td>' +
+    //   '<div class="dates-inputs">' +
+    //   '<div class="date-box">_</div>' +
+    //   '<div class="date-box">_</div>' +
+    //   '<div class="date-box">_</div>' +
+    //   '</div>'+
+    //   '</td>' +
+    //   '</tr>'
+    // )($scope));
   }
 }]);
